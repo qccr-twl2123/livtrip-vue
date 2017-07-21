c2d5a4680222aca2d52423504b3f5eafafb1f97e
mark1xie <54355534453@qq.com>
2017-07-21 18:27:05 +0800

    add header

--- src/app.vue
+++ src/app.vue
@@ -1,24 +1,40 @@
 <style scoped>
     @import 'styles/common.css';
+    @import 'http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css';
 </style>
 <template>
-    <div>
+    <div id="app">
+
+        <v-head></v-head>
         <router-view></router-view>
     </div>
 </template>
 <script>
-    export default {
-        data () {
-            return {}
+import vHead from './components/public/head.vue'
+export default {
+    components: {
+        vHead,
+        'remote-js': {
+            render(createElement) {
+                return createElement('script', { attrs: { type: 'text/javascript', src: this.src }});
+            },
+            props: {
+                src: { type: String, required: true },
+            },
         },
-        mounted () {
+    },
+    data () {
+        return {}
+    },
+    mounted () {
 
-        },
-        beforeDestroy () {
+    },
+    beforeDestroy () {
 
-        },
-        methods: {
+    },
+    methods: {
 
-        }
     }
+
+}
 </script>
